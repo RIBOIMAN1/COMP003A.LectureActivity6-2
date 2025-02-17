d4a8579c195ca449bdf67bfc3f5c7d2790002e1c
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-17 01:34:58 -0800

    Added code from step 2 of the 6.4 lecture activity.

--- COMP003A.LectureActivity6-2/Program.cs
+++ COMP003A.LectureActivity6-2/Program.cs
@@ -2,7 +2,45 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+		/// <summary>
+		/// Demonstrates method overloading for different shape calculations.
+		/// </summary>
+		class ShapeCalculator
+		{
+			/// <summary>
+			/// Calculates the area of a circle.
+			/// </summary>
+			/// <param name="radius"></param>
+			/// <returns></returns>
+			public double CalculateArea(double radius)
+			{
+				return Math.PI * radius * radius;
+			}
+
+			/// <summary>
+			/// Calculates the area of a rectangle.
+			/// </summary>
+			/// <param name="length"></param>
+			/// <param name="width"></param>
+			/// <returns></returns>
+			public double CalculateArea(double length, double width)
+			{
+				return length * width;
+			}
+
+			/// <summary>
+			/// Calculates the area of a triangle.
+			/// </summary>
+			/// <param name="baseLength"></param>
+			/// <param name="height"></param>
+			/// <param name="isTriangle"></param>
+			/// <returns></returns>
+			public double CalculateArea(double baseLength, double height, bool isTriangle)
+			{
+				return 0.5 * baseLength * height;
+			}
+		}
+		static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
